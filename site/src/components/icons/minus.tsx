02e0026777e3527f34f67bca5387a492908a792d
Jed Watson <jed.watson@me.com>
2021-08-22 20:33:20 +1000

    Switching to React SVG Icons

---
+++ site/src/components/icons/minus.tsx
@@ -0,0 +1,19 @@
+import { SVGAttributes } from "react";
+
+export default function Minus(props: SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
