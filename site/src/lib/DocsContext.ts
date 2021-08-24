447845ec69b2468a14cafb47a6edef278d7f16a2
Jed Watson <jed.watson@me.com>
2021-08-24 17:05:58 +1000

    Explicit SymbolId type

--- site/src/lib/DocsContext.ts
+++ site/src/lib/DocsContext.ts
@@ -1,16 +1,18 @@
 import { createContext, useContext } from "react";
 import { SerializedSymbol } from "../extract/utils";
 
+type SymbolId = string;
+
 export type DocsContextType = {
-  symbols: Record<string, SerializedSymbol>;
-  references: Record<string, string[]>;
+  symbols: Record<SymbolId, SerializedSymbol>;
+  references: Record<SymbolId, SymbolId[]>;
   canonicalExportLocations: Record<
-    string,
-    { exportName: string; fileSymbolName: string }
+    SymbolId,
+    { exportName: string; fileSymbolName: SymbolId }
   >;
-  symbolsForInnerBit: Map<string, string[]>;
-  goodIdentifiers: Record<string, string>;
-  rootSymbols: Set<string>;
+  symbolsForInnerBit: Map<SymbolId, SymbolId[]>;
+  goodIdentifiers: Record<SymbolId, string>;
+  rootSymbols: Set<SymbolId>;
 };
 
 export const DocsContext = createContext<DocsContextType>(null as any);
