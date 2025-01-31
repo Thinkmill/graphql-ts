0c6cd636dfc5f4c6db72396c97d3d74e6a318961
Emma Hamilton <git@emmas.town>
2025-01-31 15:18:39 +1000

    Changesets and doc updates

--- packages/schema/src/schema-api-alias.d.ts
+++ packages/schema/src/schema-api-alias.d.ts
@@ -1,18 +1,28 @@
+import * as _graphql from "./schema-api";
+
 /**
  * The `graphql` export has been renamed to `g`.
  *
  * To quickly update usages of `graphql` in your project to `g`:
  *
- * 1. Use your editor's "Go to Definition" to go navigate to where this deprecated
- *    alias is defined
+ * 1. Navigate to
+ *    `node_modules/@graphql-ts/schema/dist/declarations/src/schema-api-alias.d.ts`
+ *    in your editor ("Go to Definition" will not take you to the correct file)
  * 2. Use "Rename Symbol" to rename `graphql` to `g`, this will update usages of
  *    `graphql` to `g`
  * 3. Change this deprecated alias back from `g` to `graphql` using normal text
- *    editing to preserve the updates you made in step 2
+ *    editing (avoid using "Rename Symbol" again because you want to preserve
+ *    the updates you made in step 2)
  *
- * You can similarly use "Rename Symbol" to quickly rename your own custom
- * defined `graphql` to `g` if you wish.
+ * You can also use "Rename Symbol" to quickly rename your own custom defined
+ * `graphql` export to `g`.
  *
  * @deprecated
  */
-export import graphql = require("./schema-api");
+// this is what you should run "Rename Symbol" on
+// when that is done, you should change it back to `export import graphql = _graphql;`
+// avoid using "Rename Symbol" to revert it because you want to
+// preserve the updates you made to the usages of `graphql`
+//            |||||||
+//            vvvvvvv
+export import graphql = _graphql;
