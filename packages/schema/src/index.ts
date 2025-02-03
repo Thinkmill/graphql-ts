b1562de030c7e14b793afd22a9814072daa1b467
Emma Hamilton <git@emmas.town>
2025-02-03 14:00:50 +1000

    Try another thing for docsmill

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -39,7 +39,7 @@
  *
  * @module
  */
-export * from "./schema-api-export";
+export { g } from "./schema-api-export";
 export { bindGraphQLSchemaAPIToContext } from "./output";
 export type {
   Field,
