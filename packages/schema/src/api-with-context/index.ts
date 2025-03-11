c80239f67f5dcd2c58f09b0b02c4759067ced174
Emma Hamilton <git@emmas.town>
2025-03-12 09:56:17 +1000

    Easier context binding with instantiation expressions (#42)

--- packages/schema/src/api-with-context/index.ts
+++ packages/schema/src/api-with-context/index.ts
@@ -1,8 +1,14 @@
+/**
+ * @module
+ * @deprecated This entrypoint should no longer be used. Use {@link initG}
+ *   instead.
+ */
+import { initG } from "@graphql-ts/schema";
 export {
   field,
   fields,
-  interface,
   interfaceField,
   object,
   union,
+  interface,
 } from "./api-with-context";
